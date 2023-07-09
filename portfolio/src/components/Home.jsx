c85aae13b857ad5063128bdf97e2074424a0dc98
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-07-09 16:39:51 -0500

    added to projects and home

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -15,9 +15,19 @@ const Home = () => {
                         <p className="home-title">Full-Stack Developer | Software Engineer</p>
                         <h1>Hi, I'm Chris Morales</h1>
                         <p className="home-description">
-                              Welcome to my portfolio! I'm a passionate Software Engineer with a background in education and finance. 
+                        I'm a Software Engineer who loves creating user-friendly websites and applications. With a background in education and finance, I bring a fresh perspective to problem-solving. I approach every project with a positive attitude and a commitment to learning the latest technology trends. My goal is to use my problem-solving, organization, and continuous learning skills to make a significant impact in cutting-edge software engineering.
+                        </p><br />
+                        <p> <span className="home-technologies">Front-End Technologies:</span> 
+                        HTML || CSS || Javascript || React || Next.js || Bootstrap.
+                        </p><br />
+                        <p><span className="home-technologies">Back-End Technologies:</span> 
+                        Python || Node.js || Django || MongoDB || Mongoose || PostgreSQL || Express.js
+                        </p><br />
+                        <p>
+                        <span className="home-technologies">Design Software:</span> 
+                        Figma
                         </p>
-                        <div className="tech-stack">
+                        {/* <div className="tech-stack">
                               <h2>Technologies Used:</h2>
                               <div className="tech-icons">
                                     <i class="fa-solid fa-code"></i>
@@ -29,24 +39,30 @@ const Home = () => {
                                     <i class="fa-solid fa-code"></i>
                               </div>
                         </div>
-                  <div className="tech-stack">
-                        <h2>Back-End Database Tech Used: </h2>
-                        <div className="tech-icons">
-                              <img className="icon-mongodb" src="https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png" />
-                              <img className="icon-expressjs" src="https://miro.medium.com/v2/resize:fit:1400/1*f7ztMaMM0etsFHpEfkdiwA.png"  />
-                              <img className="icon-mongoose" src="https://ih1.redbubble.net/image.438912065.6243/flat,1000x1000,075,f.u3.jpg" />
-                              <img className="icon-react-router" src="https://cdn-media-1.freecodecamp.org/images/1*TKvlTeNqtkp1s-eVB5Hrvg@2x.png" />
-                        </div>
-                  </div>
+                        <div className="tech-stack">
+                              <h2>Back-End Database Tech Used: </h2>
+                              <div className="tech-icons">
+                                    <img className="icon-mongodb" src="https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png" />
+                                    <img className="icon-expressjs" src="https://miro.medium.com/v2/resize:fit:1400/1*f7ztMaMM0etsFHpEfkdiwA.png"  />
+                                    <img className="icon-mongoose" src="https://ih1.redbubble.net/image.438912065.6243/flat,1000x1000,075,f.u3.jpg" />
+                                    <img className="icon-react-router" src="https://cdn-media-1.freecodecamp.org/images/1*TKvlTeNqtkp1s-eVB5Hrvg@2x.png" />
+                              </div>
+                        </div> */}
                   </div>
                   <div className="home-col-2">
-                        <img src="src/assets/aboutMe.jpg"/>
-                        <div id="home-icons" class="social-icons">
-                              <a href="https://github.com/christophermoraleswebdev" target="_blank"><i class="fa-brands fa-github fa-lg"></i></a>
-                              <a href="https://www.linkedin.com/in/christopher-moralesse/" target="_blank"><i class="fa-brands fa-linkedin fa-lg"></i></a>
-                              <a href="https://docs.google.com/document/d/1qO68bZuLZ0ftlELrCv_DMlp9S5dAp8oJNBVvcyPvWfo/edit?usp=sharing" target="_blank">
-                                    <button class="resume-button">View Resume</button>
-                              </a>
+                        <div className="homeImg-container">
+                              <img src="src/assets/aboutMe.png"/>
+                        </div>
+                        <div className="icons-container">
+                              <div class="home-icons">
+                                    <a href="https://github.com/christophermoraleswebdev" target="_blank"><i class="fa-brands fa-github fa-lg"></i></a>
+                                    <a href="https://www.linkedin.com/in/christopher-moralesse/" target="_blank"><i class="fa-brands fa-linkedin fa-lg"></i></a>
+                              </div>
+                              <div className="resumeButton-container">
+                                    <a href="https://docs.google.com/document/d/1qO68bZuLZ0ftlELrCv_DMlp9S5dAp8oJNBVvcyPvWfo/edit?usp=sharing" target="_blank">
+                                          <button class="resume-button">View Resume</button>
+                                    </a>
+                              </div>
                         </div>
                   </div>
             </div>
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,14 +3,16 @@ import quoteGenerator from '../assets/quoteGenerator.png'
 import samplePortfolioImage from '../assets/Sample Portfolio.png'
 import weatherApi from '../assets/Weather API.png'
 import devConnect from '../assets/DevConnect.png'
+import reactNucamp from '../assets/reactNucamp.png'
 import { motion } from 'framer-motion'
 
 const projectData = [
   { id: 1, title: 'DevConnect', imageUrl: devConnect, siteLink: 'https://www.youtube.com/watch?v=inoaW9zMyDI&t=15s', technologies: 'HTML | CSS | JavaScript | Axios | MongoDB | ExpressJS | Mongoose' },
-  { id: 2, title: 'Quote Generator', imageUrl: quoteGenerator, siteLink: 'https://christophermoraleswebdev-quotes.netlify.app/', technologies: 'HTML | CSS | JavaScript | Axios' },
+  { id: 2, title: 'Should I Buy', imageUrl: shouldIBuyImage, siteLink: 'https://should-i-buy.netlify.app/', technologies: 'HTML | CSS | JavaScript | Axios' },
   { id: 3, title: 'Sample Portfolio', imageUrl: samplePortfolioImage, siteLink: 'https://sample-portfolio-christopher-morales.netlify.app/#', technologies: 'HTML | CSS | JavaScript' },
   { id: 4, title: 'Weather App', imageUrl: weatherApi, siteLink: 'https://weatherapichristophermorales.netlify.app/', technologies: 'HTML | CSS | JavaScript | Axios' },
-  { id: 5, title: 'Should I Buy', imageUrl: shouldIBuyImage, siteLink: 'https://should-i-buy.netlify.app/', technologies: 'HTML | CSS | JavaScript | Axios' },
+  { id: 5, title: 'React Nucamp', imageUrl: reactNucamp, siteLink: 'https://react-nucamp-project.firebaseapp.com/', technologies: 'React | SASS | Firebase Deployment' },
+  { id: 6, title: 'Quote Generator', imageUrl: quoteGenerator, siteLink: 'https://christophermoraleswebdev-quotes.netlify.app/', technologies: 'HTML | CSS | JavaScript | Axios' },
 ]
 
 const Projects = () => {
